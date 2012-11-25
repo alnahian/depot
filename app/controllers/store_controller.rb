0c7b9e2be96856bca0b2167620369d1aefb88546
alnahian <nahian33@yahoo.com>
2012-11-25 11:50:45 +0600

    ch:08::added store controller, made default page, beautification of page

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -14,20 +14,9 @@
 # We make no guarantees that this code is fit for any purpose. 
 # Visit http://www.pragmaticprogrammer.com/titles/rails4 for more book information.
 #---
-class Product < ActiveRecord::Base
-  attr_accessible :title, :description, :image_url, :price
-  default_scope :order => 'title'
+class StoreController < ApplicationController
+  def index
+    @products = Product.all
+  end
 
-  # validation stuff...
-
-
-  validates :title, :description, :image_url, :presence => true
-  validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
-# 
-  validates :title, :uniqueness => true
-  validates :image_url, :format => {
-    :with    => %r{\.(gif|jpg|png)$}i,
-    :message => 'must be a URL for GIF, JPG or PNG image.'
-  }
-  validates :title, :length => {:minimum => 10}
 end
