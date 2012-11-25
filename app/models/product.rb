1cb7a7b7353f1f082bd086d5e56e45a8878bbc6c
alnahian <nahian33@yahoo.com>
2012-11-25 18:09:47 +0600

    ch:09::created line items controller,scaffold;added button for cart

--- app/models/product.rb
+++ app/models/product.rb
@@ -17,6 +17,12 @@
 class Product < ActiveRecord::Base
   attr_accessible :title, :description, :image_url, :price
   default_scope :order => 'title'
+  has_many :line_items
+
+  before_destroy :ensure_not_referenced_by_any_line_item
+
+  #...
+
 
   # validation stuff...
 
@@ -30,4 +36,15 @@ class Product < ActiveRecord::Base
     :message => 'must be a URL for GIF, JPG or PNG image.'
   }
   validates :title, :length => {:minimum => 10}
+  private
+
+    # ensure that there are no line items referencing this product
+    def ensure_not_referenced_by_any_line_item
+      if line_items.empty?
+        return true
+      else
+        errors.add(:base, 'Line Items present')
+        return false
+      end
+    end
 end
