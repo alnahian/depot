dca2cea06a7f26e56be82ffcf079952f185f3de1
alnahian <nahian33@yahoo.com>
2012-12-10 22:32:41 +0600

    ch:15::completed;internationalization;added spanish language with english

--- config/initializers/i18n.rb
+++ config/initializers/i18n.rb
@@ -14,21 +14,10 @@
 # We make no guarantees that this code is fit for any purpose. 
 # Visit http://www.pragmaticprogrammer.com/titles/rails4 for more book information.
 #---
-Depot::Application.routes.draw do
-  get 'admin' => 'admin#index'
-  controller :sessions do
-    get  'login' => :new
-    post 'login' => :create
-    delete 'logout' => :destroy
-  end
-  scope '(:locale)' do
-    resources :users
-    resources :orders
-    resources :line_items
-    resources :carts
-    resources :products do
-      get :who_bought, :on => :member
-    end
-    root :to => 'store#index', :as => 'store'
-  end
-end
+#encoding: utf-8
+I18n.default_locale = :en
+
+LANGUAGES = [
+  ['English',                  'en'],
+  ["Espa&ntilde;ol".html_safe, 'es']
+]
